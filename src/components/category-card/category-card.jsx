8d6b1f1418a33bf4b6fa6aaa9421db6db9ff32a7
Stefan30 <secrieru23022@gmail.com>
2023-04-05 23:33:34 +0300

    Change design of categories

--- src/components/category-card/category-card.jsx
+++ src/components/category-card/category-card.jsx
@@ -1,23 +1,48 @@
 import './category-card.css'
 import {useEffect, useState} from "react";
 import imgWEB from "../../utils/imgs/landing-page/smalls/WebDev.svg";
+import imgWEBCover from "../../utils/imgs/WebDevCover.jpg";
 import imgNetAdm from "../../utils/imgs/landing-page/smalls/NetAdmin.svg";
+import imgNetAdmCover from "../../utils/imgs/NetworksCover.jpg";
 import imgSoftEng from "../../utils/imgs/landing-page/smalls/SoftwareEng.svg";
-import Divider from "../divider/divider";
+import imgSoftEngCover from "../../utils/imgs/SoftwareEngCover.jpg";
+import imgAI from "../../utils/imgs/landing-page/smalls/AILogo.svg";
+import imgAICover from "../../utils/imgs/AICover.jpg";
+import imgBigData from "../../utils/imgs/landing-page/smalls/BigData.svg";
+import imgBigDataCover from "../../utils/imgs/BigDataCover.jpg";
+import imgSecurity from "../../utils/imgs/landing-page/smalls/Security.svg";
+import imgSecurityCover from "../../utils/imgs/SecurityCover.jpg";
+
 const CategoryCard = ({cat}) => {
 
     const [img, setImg] = useState(null)
+    const [coverImg, setCoverImg] = useState(null)
 
     useEffect(() => {
-        switch (cat){
-            case 'WEB':
+        switch (cat) {
+            case 'WEB Development':
                 setImg(imgWEB)
+                setCoverImg(imgWEBCover)
                 break
             case 'Network Administration':
                 setImg(imgNetAdm)
+                setCoverImg(imgNetAdmCover)
                 break
             case 'Software Engineering':
                 setImg(imgSoftEng)
+                setCoverImg(imgSoftEngCover)
+                break
+            case 'Artificial Intelligence':
+                setImg(imgAI)
+                setCoverImg(imgAICover)
+                break
+            case 'Big Data':
+                setImg(imgBigData)
+                setCoverImg(imgBigDataCover)
+                break
+            case 'Cyber Security':
+                setImg(imgSecurity)
+                setCoverImg(imgSecurityCover)
                 break
         }
     }, [])
@@ -27,8 +52,10 @@ const CategoryCard = ({cat}) => {
     }
 
     return (
-        <div onMouseEnter={displayCategoryName} className='category-card-container'>
+        <div onMouseEnter={displayCategoryName} style={{backgroundImage: `URL(${coverImg})`}}
+             className='category-card-container'>
             <img className='category-icon' src={img} alt={cat}/>
+            <p>{cat}</p>
             {/*<Divider />*/}
             {/*<p>{cat}</p>*/}
         </div>
